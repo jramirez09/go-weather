7ca423d92ccef757d0776e196e3d1cfdfc052fa8
jramirez09 <jramirez12@luc.edu>
2019-07-26 15:57:38 -0500

    testing3

---
+++ api/api.go
@@ -0,0 +1,8 @@
+package api
+
+import "fmt"
+
+// SayHello prints out someone's name.
+func SayHello(name string) {
+	fmt.Printf("Hello %s\n", name)
+}
--- main.go
+++ main.go
@@ -7,8 +7,3 @@ func main() {
 		fmt.Println("Hello, World!")
 	}
 }
-
-// SayHello prints out someone's name.
-func SayHello(name string) {
-	fmt.Printf("Hello %s\n", name)
-}
