6995357d006c04abfe4282ae33241a282f9f1224
jramirez09 <jramirez12@luc.edu>
2019-07-26 15:54:41 -0500

    matata

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-  for i := 0; i < 5; i++ {
-    fmt.Println("Hello, World!")
-  }
+	for i := 0; i < 5; i++ {
+		fmt.Println("Hello, World!")
+	}
 }
 
+// SayHello prints out someone's name.
+func SayHello(name string) {
+	fmt.Printf("Hello %s\n", name)
+}
